e562ecc9236fd3cc88baec9034db5a4c662661bb
Meas Soron <91657834+Soron123@users.noreply.github.com>
2025-07-14 20:27:09 +0700

    add new feature

---
+++ target/generated-sources/annotations/com/example/springbootapp/mapper/ProductMapperImpl.java
@@ -0,0 +1,127 @@
+package com.example.springbootapp.mapper;
+
+import com.example.springbootapp.dto.ProductDto;
+import com.example.springbootapp.entity.Category;
+import com.example.springbootapp.entity.Product;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-07-14T07:03:22+0700",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 24.0.1 (Oracle Corporation)"
+)
+@Component
+public class ProductMapperImpl implements ProductMapper {
+
+    @Override
+    public ProductDto toDto(Product product) {
+        if ( product == null ) {
+            return null;
+        }
+
+        ProductDto productDto = new ProductDto();
+
+        productDto.setCategoryId( productCategoryId( product ) );
+        productDto.setCategoryName( productCategoryName( product ) );
+        productDto.setId( product.getId() );
+        productDto.setName( product.getName() );
+        productDto.setDescription( product.getDescription() );
+        productDto.setPrice( product.getPrice() );
+        productDto.setStockQuantity( product.getStockQuantity() );
+        productDto.setActive( product.isActive() );
+
+        return productDto;
+    }
+
+    @Override
+    public Product toEntity(ProductDto dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Product product = new Product();
+
+        product.setCategory( productDtoToCategory( dto ) );
+        product.setId( dto.getId() );
+        product.setName( dto.getName() );
+        product.setDescription( dto.getDescription() );
+        product.setPrice( dto.getPrice() );
+        product.setStockQuantity( dto.getStockQuantity() );
+        product.setActive( dto.isActive() );
+
+        return product;
+    }
+
+    @Override
+    public List<ProductDto> toDtoList(List<Product> products) {
+        if ( products == null ) {
+            return null;
+        }
+
+        List<ProductDto> list = new ArrayList<ProductDto>( products.size() );
+        for ( Product product : products ) {
+            list.add( toDto( product ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public List<Product> toEntityList(List<ProductDto> dtos) {
+        if ( dtos == null ) {
+            return null;
+        }
+
+        List<Product> list = new ArrayList<Product>( dtos.size() );
+        for ( ProductDto productDto : dtos ) {
+            list.add( toEntity( productDto ) );
+        }
+
+        return list;
+    }
+
+    private Long productCategoryId(Product product) {
+        if ( product == null ) {
+            return null;
+        }
+        Category category = product.getCategory();
+        if ( category == null ) {
+            return null;
+        }
+        Long id = category.getId();
+        if ( id == null ) {
+            return null;
+        }
+        return id;
+    }
+
+    private String productCategoryName(Product product) {
+        if ( product == null ) {
+            return null;
+        }
+        Category category = product.getCategory();
+        if ( category == null ) {
+            return null;
+        }
+        String name = category.getName();
+        if ( name == null ) {
+            return null;
+        }
+        return name;
+    }
+
+    protected Category productDtoToCategory(ProductDto productDto) {
+        if ( productDto == null ) {
+            return null;
+        }
+
+        Category category = new Category();
+
+        category.setId( productDto.getCategoryId() );
+
+        return category;
+    }
+}
